ed9afdca5ae5f17917788458b90a40715910d417
aoarashi <dodolo321@sina.com>
2018-12-03 15:56:27 +0800

    增加更完善的log提示

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ app.use(cors())
 
 function getImage(originalUrl) {
   const path = formatUrl(originalUrl)
-  console.log('接收到图片资源请求，路径 = ', path)
+  console.log('\n接收到图片资源请求，路径 = ', path)
   let data
   try {
-    console.log('正在读取本地文件...')
+    console.log('\n正在读取本地文件...')
     data = fs.readFileSync(path)
   } catch (error) {
-    throw '文件不存在，请检查charactors文件夹内容'
+    throw '\n文件不存在，请检查charactors文件夹内容'
   }
-  console.log('本地文件读取完毕，正在加密...')
+  console.log('\n本地文件读取完毕，正在加密...')
   const encodeBuffer = util.XOR(data)
-  console.log('加密完毕，返回加密后图片')
+  console.log('\n加密完毕，返回加密后图片')
   return encodeBuffer
 }
 
@@ -56,9 +56,9 @@ function getVersionJson() {
       let error
 
       if (200 !== statusCode) {
-        error = new Error(`请求失败\n状态码：${statusCode}`)
+        error = new Error(`\n请求失败\n状态码：${statusCode}`)
       } else if (!/^application\/json/.test(contentType)) {
-        error = new Error(`非法的 content-type\n期待的是 application/json 但接受到的是 ${contentType}`)
+        error = new Error(`\n非法的 content-type\n期待的是 application/json 但接受到的是 ${contentType}`)
       }
 
       if (error) {
@@ -76,13 +76,13 @@ function getVersionJson() {
       res.on('end', () => {
         try {
           const parsedData = JSON.parse(rawData)
-          console.log('\n获取成功')
+          console.log('\n获取 version.json 成功')
           resolve(parsedData)
         } catch (err) {
-          reject(`转换version.json失败: ${err.message}`)
+          reject(`\n转换version.json失败: ${err.message}`)
         }
       }).on('error', e => {
-        reject(`报错： ${e.message}`)
+        reject(`\n报错： ${e.message}`)
       })
     })
   })
@@ -100,7 +100,7 @@ function getCodeJS(versionJson) {
       const { statusCode } = res
 
       if (200 !== statusCode) {
-        reject('获取code.js失败')
+        reject('\n获取code.js失败')
       }
 
       res.setEncoding('utf8')
@@ -112,7 +112,7 @@ function getCodeJS(versionJson) {
       })
 
       res.on('end', () => {
-        console.log('\n获取成功')
+        console.log('\n获取 原始code.js 成功 ')
         resolve(codeJS)
       })
     })
@@ -150,7 +150,7 @@ function readBgmJS(codeJS) {
       resolve({ codeJS, bgmJS })
     }
 
-    reject('/script/bgm.js 文件不存在， 你可以前往 https://github.com/aoarashi1988/majsoul_custrom_charactor 下载对应文件')
+    reject('\n/script/bgm.js 文件不存在， 你可以前往 https://github.com/aoarashi1988/majsoul_custrom_charactor 下载对应文件')
   })
 }
 
@@ -164,6 +164,7 @@ function writeCodePatchedJS({ codeJS, bgmJS }) {
   fs.writeFileSync(path, codePatchedJS)
 
   console.log('\n写入完毕')
+  console.log('\n本地服务器初始化完成，祝君游戏愉快 \\(^o^)/~')
 }
 
 getVersionJson()
@@ -174,13 +175,19 @@ getVersionJson()
   .catch(console.log)
 
 function initDir(dir) {
+  console.log(`\n检查 /${dir} 目录是否存在...`)
   fs.exists(__dirname + `/${dir}`, exists => {
     if (!exists) {
+      console.log(`\n/${dir}目录不存在，正在尝试创建...`)
       fs.mkdir(__dirname + `/${dir}`, err => {
         if (err) {
-          console.log(`创建 /${dir} 目录失败，请手动创建`)
+          console.log(`\n/${dir} 目录创建失败，请手动创建`)
+        } else {
+          console.log(`\n/${dir} 目录创建成功`)
         }
       })
+    } else {
+      console.log(`\n /${dir} 目录已存在，无需重复创建`)
     }
   })
 }
